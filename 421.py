354ea78fec2abd19ba3539d88cadb04de7508bc6
Guillaume ROUCHEUX <guillaume.roucheux@tutanota.com>
2022-04-05 08:38:11 +0200

    Fichier vaseu

--- 421.py
+++ 421.py
@@ -4,45 +4,45 @@ class Jeu:
 	def __init__(self):
 		print("Binvenue sur le jeu du 421 \n Pour jouer tapez 1, Pour quitter tapez 2")
 		choix = input()
-		demanderNombreJoueurs(choix)
+		self.demanderNombreJoueurs()
 
-	def lancerJeu(self, nombreJoueur):
-		nombreJoueurs = int(nombre_joueurs)
+	def lancerJeu(self, nombreDeJoueurs):
+		print(nombreDeJoueurs)
 		relancer = 0
 		while relancer != 2:
 			joueur = 0
-			for i in range(nombreJoueurs):
+			for i in range(nombreDeJoueurs):
 				joueur +=1
-				des = Des()
-				resultatJoueur = des.lancer_des()
+				resultatJoueur = self.Des()
 				print("Le joueur ", joueur, "a fait ",resultatJoueur)
-				resultat = joueurs.Joueur_gagnant(resultatJoueur)
+				resultat = self.Joueur_gagnant(resultatJoueur)
 				if resultat == True:
 					print("Le joueur", joueur, "a gagné.\n 1: relancer 2: Quitter" )
 					relancer = input()
-				if relancer == "2":
-					exit()
+					if relancer == "2":
+						exit()
 
-	def demanderNombreJoueurs(self, choix):
-		if choix == 1:
-			print("Entrez le nombre de joueurs et definissez vous un numero")
-			nombre_joueurs = input()
-			joueurs = Joueur(nombre_joueurs)
-			joueurs.afficherNombreJoueurs()
+				else:
+					print("Pas de gagnant pour ce tour.\n 1: Relancer 2: Quitter")
+					relancer = input()
+					if relancer == "2":
+						exit()
+
+	def demanderNombreJoueurs(self):
+		print("Entrez le nombre de joueurs et definissez vous un numero")
+		nombre_joueurs = input()
+		joueurs = Joueur(nombre_joueurs)
+		joueurs.afficherNombreJoueurs()
 
 
-		print("Pas de gagnant pour ce tour.\n 1: Relancer 2: Quitter")
-		relancer = input()
-		if relancer == "2":
-			exit()
-
-
-class Joueur:
-	def __init__(self, nombreJoueurs):
-			self.nombreJoueurs = nombreJoueurs
+class Joueur(Jeu):
+	def __init__(self, nombre_Joueurs):
+		self.nombreJoueurs = nombre_Joueurs
 
 	def afficherNombreJoueurs(self):
-		print("Vous serez ", self.nombreJoueurs, "joueurs")
+		print("Vous serez ", self.nombre_Joueurs, "joueurs")
+		nombreJoueurs = int(nombre_Joueurs)
+		self.lancerJeu(nombreJoueurs)
 
 	def Joueur_gagnant(self, resultatJoueur):
 		gagnant = [[4,2,1], [4,1,2], [2,1,4], [2,4,1], [1,2,4], [1,4,2]]
@@ -70,43 +70,17 @@ class Joueur:
 		return Joueurs
 		
 
-class Des:
-    def __init__(self):
-    	pass
-
-    def lancer_des(self):
-    	score = []
-    	for tableau in range (3):
-        	valeur_de = randint(1, 6)
-        	score.append(valeur_de)
+class Des(Jeu):
+	def __init__ (self):
+		score = []
+		for tableau in range(3):
+			valeur_de = randint(1, 6)
+			score.append(valeur_de)
             
-    	return score
+		return score
 
 
-
-
-
-
-	print("1: lancer les dés 2: Quitter")
-	choix = input()
-	if choix.isdigit():
-		if choix != 1 or choix != 2:
-			print:("Not 1 or 2")
-			quit()
-
-		else:
-			lancerJeu(nombre_joueurs)
-			
-
-	else:
-		print("On avait dit un NOMBRE de joueurs")
-		quit()
-	
-	
-
-else:
-	quit()
-
+Jeu()
 
 
 
